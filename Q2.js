f2d9c30e7afeb83aae6266edf323e432627f5066
Zeb5 <blackburn.ze@oundleschool.org.uk>
2024-09-17 09:41:20 +0100

    dice

---
+++ Q2.js
@@ -0,0 +1,7 @@
+function addition(){
+    var x = document.getElementById("num1").value;
+    var y = document.getElementById("num2").value;
+    var z = x + y;
+    output=document.getElementById("output");
+    output.innerHTML=z;
+}
